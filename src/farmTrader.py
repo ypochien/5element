ba9e16f36381205488f40bcf2c3713cfe418b0a6
ypochien <ypochien@gmail.com>
2019-08-16 21:04:30 +0800

    改用load_ui

--- src/farmTrader.py
+++ src/farmTrader.py
@@ -1,25 +1,25 @@
 import sys
+from PySide2.QtUiTools import QUiLoader
 from PySide2 import QtCore, QtGui, QtWidgets
 from PySide2.QtWidgets import QApplication, QMainWindow
-from uifarmtrade import Ui_MainWindow
+from PySide2.QtCore import QObject, Signal, Slot,QFile
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        pixmap = QtGui.QPixmap("img/shioaji.png")
-        lbl = QtWidgets.QLabel()
-        lbl.setAlignment(QtCore.Qt.AlignRight)
-        lbl.setPixmap(pixmap)
-        self.ui.horizontalLayout.addWidget(lbl)
-        self.setWindowIcon(QtGui.QIcon('img/shioaji.png')) 
-
-
+def set_init(MainWindow):
+    pixmap = QtGui.QPixmap("img/shioaji.png")
+    lbl = QtWidgets.QLabel()
+    lbl.setAlignment(QtCore.Qt.AlignRight)
+    lbl.setPixmap(pixmap)
+    MainWindow.horizontalLayout.addWidget(lbl)
+    MainWindow.setWindowIcon(QtGui.QIcon('img/shioaji.png')) 
+        
 if __name__ == "__main__":
+    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
     app = QApplication(sys.argv)
-
-    window = MainWindow()
+    ui_file = QFile("ui/farmtrade.ui")
+    ui_file.open(QFile.ReadOnly)
+    loader = QUiLoader()
+    window = loader.load(ui_file)
+    set_init(window)
+    ui_file.close()
     window.show()
-
     sys.exit(app.exec_())
