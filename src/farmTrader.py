8158ad30b35ed96894a25ee26e67d68c75a7f5f6
ypochien <ypochien@gmail.com>
2019-08-18 22:44:52 +0800

    for bidask

--- src/farmTrader.py
+++ src/farmTrader.py
@@ -1,18 +1,19 @@
 import sys
 from PySide2.QtUiTools import QUiLoader
 from PySide2 import QtCore, QtGui, QtWidgets
-from PySide2.QtWidgets import QApplication, QMainWindow, QWidget
+from PySide2.QtWidgets import QApplication, QMainWindow, QWidget, QTableWidgetItem
 from PySide2.QtCore import QObject, Signal, Slot, QFile
 from ui_mainWindow import Ui_MainWindow
 from ui_trade import Ui_Form
-import json
 import shioaji as sj
+import asyncio
 
 
-class Communicate(QObject):
+class RTUpdate(QObject):
     # create two new signals on the fly: one will handle
     # int type, the other will handle strings
-    speak = Signal((list,))
+    update_price_caller = Signal((list,))
+    update_bidask_caller = Signal((list,))
 
 
 class mainUI(QMainWindow, Ui_MainWindow):
@@ -25,9 +26,10 @@ class mainUI(QMainWindow, Ui_MainWindow):
         self.setWindowIcon(QtGui.QIcon("img/shioaji.png"))
         # event
         self.trade.login_button.clicked.connect(self.login)
-        self.someone = Communicate()
-        self.someone.speak.connect(self.say_something)
-        self.api = sj.Shioaji()
+        self.rtUpdate = RTUpdate()
+        self.rtUpdate.update_price_caller.connect(self.update_price_worker)
+        self.rtUpdate.update_bidask_caller.connect(self.update_bidask_worker)
+        # self.api = sj.Shioaji()
 
     def logo(self):
         pixmap = QtGui.QPixmap("img/shioaji.png")
@@ -37,7 +39,6 @@ class mainUI(QMainWindow, Ui_MainWindow):
         return lbl
 
     def quote_msg(self, topic, msg):
-        print(topic, msg)
         lst = [
             msg["Code"],
             msg["Close"][0],
@@ -45,24 +46,62 @@ class mainUI(QMainWindow, Ui_MainWindow):
             msg["Volume"][0],
             msg["VolSum"][0],
         ]
-        self.someone.speak.emit(lst)
+        self.rtUpdate.update_price_caller.emit(lst)
+
+    def bidask_msg(self, topic, msg):
+        lst = list(range(0, 20))
+        print(lst)
+        self.rtUpdate.update_bidask_caller.emit(lst)
 
     @Slot(list)
-    def say_something(self, lst):
+    def update_bidask_worker(self, lst):
+        tableWidget = self.trade.bidask_grid
+        # item = tableWidget.item(row,column)
+        for row, i in enumerate(lst[:5]):
+            item = QTableWidgetItem()
+            item.setText(str(i))
+            tableWidget.setItem(row, 0, item)
+        for row, i in enumerate(lst[5:10]):
+            item = QTableWidgetItem()
+            item.setText(str(i))
+            tableWidget.setItem(row, 1, item)
+        for row, i in enumerate(lst[10:15]):
+            item = QTableWidgetItem()
+            item.setText(str(i))
+            tableWidget.setItem(row, 2, item)
+        for row, i in enumerate(lst[15:20]):
+            item = QTableWidgetItem()
+            item.setText(str(i))
+            tableWidget.setItem(row, 3, item)
+
+    @Slot(list)
+    def update_price_worker(self, lst):
         self.trade.code_edit.setText(lst[0])
         self.trade.curr_price.display(lst[1])
         self.trade.diff_price.display(lst[2])
         self.trade.tick_vol.display(lst[3])
         self.trade.total_vol.display(lst[4])
-        print(lst)
 
     @Slot()
     def login(self):
-        with open("user.json") as json_file:
-            user = json.load(json_file)
-        self.api.login(user["uid"], user["password"])
+        self.bidask_msg(None, None)
+        return
+        user = {
+            "uid": self.trade.id_edit.text(),
+            "password": self.trade.password_edit.text(),
+        }
+        try:
+            self.api.login(user["uid"], user["password"])
+        except asyncio.TimeoutError:
+            self.trade.code_edit.setText("目前暫停服務")
+            self.trade.code_edit.repaint()
+            return
         self.trade.login_button.setText("已登入")
+        self.trade.login_button.repaint()
         self.api.quote.subscribe(self.api.Contracts.Futures["TXFH9"])
+        self.api.qoute.subscribe(
+            self.api.Contracts.Futures["TXFH9"], quote_type="bidask"
+        )
         self.api.quote.set_callback(self.quote_msg)
 
 
---
+++ src/ui_mainWindow.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui/mainWindow.ui',
+# licensing of 'ui/mainWindow.ui' applies.
+#
+# Created: Sat Aug 17 12:28:56 2019
+#      by: pyside2-uic  running on PySide2 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide2 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(620, 374)
+        font = QtGui.QFont()
+        font.setFamily("Apple LiGothic")
+        MainWindow.setFont(font)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.centralwidget.sizePolicy().hasHeightForWidth())
+        self.centralwidget.setSizePolicy(sizePolicy)
+        self.centralwidget.setMouseTracking(False)
+        self.centralwidget.setAutoFillBackground(True)
+        self.centralwidget.setObjectName("centralwidget")
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.horizontalLayout_2.addLayout(self.horizontalLayout)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar()
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 620, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QtWidgets.QApplication.translate("MainWindow", "Shioaji - Trader", None, -1))
+
